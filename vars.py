8647d84b175093417e135c38eb19ff9ac5cfa006
knox0000 <durgamfans212@gmail.com>
2025-04-04 05:53:04 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = "15541919"
 api_hash = "fbf0d590575bbaaad9d773d04c8872e4"
-bot_token = "7706405139:AAFGBlsK8Tpfn_Fj5U6OYTQfQB403Y1xM1U"
+bot_token = "7891103245:AAFRRFH47nHHSJg2zTbyrv25yjt46m2L174"
